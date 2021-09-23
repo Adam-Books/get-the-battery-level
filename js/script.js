e9cc072637748e374772af8b49cd9872b1f2ad73
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2021-09-23 09:05:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-navigator.getBattery().then((battery) => {
-  percentage.style.width = battery.level * 100 + "%";
-  percent.innerHTML = battery.level * 100 + "%";
-});
-
 theme.onclick = () => {
   theme.classList.toggle("fa-sun");
   if (theme.classList.contains("fa-sun"))
     document.querySelector("body").classList.add("active");
   else document.querySelector("body").classList.remove("active");
 };
+
+navigator.getBattery().then((battery) => {
+  percentage.style.width = battery.level * 100 + "%";
+  percent.innerHTML = Math.round(battery.level * 100) + "%";
+});
